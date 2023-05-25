325a3000da687e8003528d1a0c09a83ea19e8b5b
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-25 11:28:24 +0300

    create and implement fun createMarket

--- src/main/kotlin/com/the_chance/data/services/MarketService.kt
+++ src/main/kotlin/com/the_chance/data/services/MarketService.kt
@@ -1,7 +1,26 @@
 package com.the_chance.data.services
 
+import com.the_chance.data.models.Market
+import com.the_chance.data.tables.MarketTable
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
-class MarketService(database:Database):BaseService(){
+class MarketService(database: Database) : BaseService() {
+    init {
+        transaction(database) {
+            SchemaUtils.create(MarketTable)
+        }
+    }
 
+    suspend fun createMarket(marketName: String): Market = dbQuery {
+        val newMarket = MarketTable.insert {
+            it[name] = marketName
+        }
+        Market(
+            id = newMarket[MarketTable.id].value,
+            name = newMarket[MarketTable.name]
+        )
+    }
 }
