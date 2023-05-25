a9aa77306223a01a681989d11c78c502f85a795f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-25 11:43:31 +0300

    implement market end point create method

--- src/main/kotlin/com/the_chance/endpoints/Markets.kt
+++ src/main/kotlin/com/the_chance/endpoints/Markets.kt
@@ -1,2 +1,27 @@
 package com.the_chance.endpoints
 
+import com.the_chance.data.ServerResponse
+import com.the_chance.data.services.MarketService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.marketsRoutes(marketService: MarketService) {
+    post("/markets") {
+        val marketName = call.receiveParameters()["name"]?.trim().orEmpty()
+        if (marketName.length < 4) {
+            call.respond(
+                HttpStatusCode.BadRequest,
+                ServerResponse.error("Market name length should be 4 characters or more")
+            )
+        } else {
+            val newMarket = marketService.createMarket(marketName)
+            call.respond(
+                HttpStatusCode.Created,
+                ServerResponse.success(newMarket, "Market created Successfully")
+            )
+        }
+    }
+}
