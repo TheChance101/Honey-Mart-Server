28b6006e271462d7893da3d852b95848a6aaa597
Nada Feteiha <nada.feteiha@gmail.com>
2023-05-25 05:59:10 -0700

    update product.

--- src/main/kotlin/com/the_chance/data/models/Product.kt
+++ src/main/kotlin/com/the_chance/data/models/Product.kt
@@ -6,5 +6,7 @@ import kotlinx.serialization.Serializable
 data class Product(
     val id: String,
     val name: String,
+    val quantity: String?,
     val price: Double,
+    val isDeleted: Boolean = false
 )
