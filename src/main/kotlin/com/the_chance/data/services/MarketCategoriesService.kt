e53cbb073885cdca62f8e62da7cdb4c45fa7d4b5
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-28 16:21:45 +0300

    enhance getAllMarketsWithCategories and remove unnecessary inner join

--- src/main/kotlin/com/the_chance/data/services/MarketCategoriesService.kt
+++ src/main/kotlin/com/the_chance/data/services/MarketCategoriesService.kt
@@ -1,30 +1,42 @@
 package com.the_chance.data.services
 
 import com.the_chance.data.models.Category
+import com.the_chance.data.models.MarketWithCategories
 import com.the_chance.data.tables.CategoriesTable
 import com.the_chance.data.tables.MarketTable
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 
 class MarketCategoriesService(database: Database) : BaseService(database, MarketTable, CategoriesTable) {
 
-    suspend fun getCategoriesByMarket(marketId: Long): List<Category> = dbQuery {
-        CategoriesTable
-            .join(MarketTable, JoinType.INNER, additionalConstraint = {
-                CategoriesTable.marketId eq MarketTable.id
-            })
+    suspend fun getAllMarketsWithCategories(): List<MarketWithCategories> = dbQuery {
+        val markets = MarketTable
             .select {
-                (CategoriesTable.marketId eq marketId) and
-                        (CategoriesTable.isDeleted eq false) and
-                        (MarketTable.isDeleted eq false)
+                MarketTable.isDeleted eq false
+            }
+            .map {
+                val marketId = it[MarketTable.id].value
+                val marketName = it[MarketTable.name]
+                MarketWithCategories(
+                    marketId = marketId,
+                    marketName = marketName,
+                    categories = getCategoriesByMarket(marketId)
+                )
+            }
+
+        markets
+    }
+
+    private suspend fun getCategoriesByMarket(marketId: Long): List<Category> = dbQuery {
+        CategoriesTable
+            .select {
+                (CategoriesTable.marketId eq marketId) and (CategoriesTable.isDeleted eq false)
             }
             .map {
                 Category(
                     categoryId = it[CategoriesTable.id].value,
-                    categoryName = it[CategoriesTable.name],
-                    marketId = it[CategoriesTable.marketId].value,
+                    categoryName = it[CategoriesTable.name]
                 )
             }
     }
