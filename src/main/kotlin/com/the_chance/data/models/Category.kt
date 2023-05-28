dc9a5962c3c646d0a91cdd1dabeadf8b07e4622c
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-28 16:17:59 +0300

    remove unused data type

--- src/main/kotlin/com/the_chance/data/models/Category.kt
+++ src/main/kotlin/com/the_chance/data/models/Category.kt
@@ -5,7 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Category(
     val categoryId: Long,
-    val marketId : Long,
     val categoryName: String,
-    val products:List<Product> = emptyList()
 )
