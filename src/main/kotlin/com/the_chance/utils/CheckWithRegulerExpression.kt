0368bc3bfbc599862c534d8d44948b2482be3cd9
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-29 00:48:41 +0300

    transfer function isValidMarketName

--- src/main/kotlin/com/the_chance/utils/CheckWithRegulerExpression.kt
+++ src/main/kotlin/com/the_chance/utils/CheckWithRegulerExpression.kt
@@ -7,3 +7,9 @@ fun isValidStringInput(name: String): Boolean {
     val matcher = pattern.matcher(name)
     return matcher.matches()
 }
+
+fun isValidMarketName(name: String): Boolean {
+    val pattern = Pattern.compile("^[a-zA-Z0-9]+(\\s[a-zA-Z0-9]+)*$")
+    val matcher = pattern.matcher(name)
+    return matcher.matches()
+}
