dd892f9221a1d34256c91cfecd22cc744b37c009
Bareq <bareqaltaamah@gmail.com>
2023-05-29 20:10:55 +0300

    reset port to 8080

--- src/main/kotlin/com/the_chance/Application.kt
+++ src/main/kotlin/com/the_chance/Application.kt
@@ -13,7 +13,7 @@ import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
 
 fun main() {
-    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
