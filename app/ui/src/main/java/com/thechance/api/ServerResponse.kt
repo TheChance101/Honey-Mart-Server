69a9eb984b4f2c9e959df0ee6b5c6e14c5b340dd
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-31 16:31:04 +0300

    add handleError in ServerResponse

--- app/ui/src/main/java/com/thechance/api/ServerResponse.kt
+++ app/ui/src/main/java/com/thechance/api/ServerResponse.kt
@@ -1,5 +1,9 @@
 package com.thechance.api
 
+import com.thechance.api.utils.IdNotFoundException
+import com.thechance.api.utils.InvalidInputException
+import com.thechance.api.utils.ItemNotAvailableException
+
 @kotlinx.serialization.Serializable
 data class ServerResponse<T>(
     val value: T?,
@@ -23,6 +27,31 @@ data class ServerResponse<T>(
                 isSuccess = true,
                 message = successMessage
             )
+        }
+
+        fun handleError(error: Exception): ServerResponse<Nothing> {
+            return when (error) {
+                is InvalidInputException -> ServerResponse(
+                    value = null,
+                    isSuccess = false,
+                    message = error.message
+                )
+                is IdNotFoundException -> ServerResponse(
+                    value = null,
+                    isSuccess = false,
+                    message = error.message
+                )
+                is ItemNotAvailableException -> ServerResponse(
+                    value = null,
+                    isSuccess = false,
+                    message = error.message
+                )
+                else -> ServerResponse(
+                    value = null,
+                    isSuccess = false,
+                    message = error.message ?: "An unknown error occurred"
+                )
+            }
         }
 
     }
