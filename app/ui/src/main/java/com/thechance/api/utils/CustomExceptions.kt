dcc1e4428d899e9328c89b96a21b94163ee36d1c
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-31 16:35:04 +0300

    refactor and add custom Exception classes

---
+++ app/ui/src/main/java/com/thechance/api/utils/CustomExceptions.kt
@@ -0,0 +1,7 @@
+package com.thechance.api.utils
+
+class InvalidInputException(message: String) : Exception(message)
+class IdNotFoundException(message: String) : Exception(message)
+class ItemNotAvailableException(message: String) : Exception(message)
+
+
