95ac5efc9f25f85695afe3caca882255259d2dc0
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-01 09:15:13 -0700

    solve conflicts

--- src/main/kotlin/com/the_chance/di/Module.kt
+++ src/main/kotlin/com/the_chance/di/Module.kt
@@ -1,18 +1,28 @@
 package com.the_chance.di
 
-import com.thechance.api.service.CategoryService
-import com.thechance.api.service.MarketCategoriesService
-import com.thechance.api.service.MarketService
-import com.thechance.api.service.ProductService
-import com.thechance.core.data.*
+import com.thechance.api.service.*
+import com.thechance.core.data.DeleteAllTablesServiceServiceImp
+import com.thechance.core.data.database.CoreDataBase
+import com.thechance.core.data.database.CoreDataBaseImp
+import com.thechance.core.data.service.CategoryServiceImp
+import com.thechance.core.data.service.MarketCategoriesServiceImp
+import com.thechance.core.data.service.MarketServiceImp
+import com.thechance.core.data.service.ProductServiceImp
+import com.thechance.core.data.validation.category.CategoryValidation
+import com.thechance.core.data.validation.category.CategoryValidationImpl
+import com.thechance.core.data.validation.product.ProductValidation
+import com.thechance.core.data.validation.product.ProductValidationImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val myModule = module {
     single<CoreDataBase> { CoreDataBaseImp() }
-    singleOf(::ProductServiceImp){bind<ProductService>()} // good job ya noor
+    singleOf(::ProductServiceImp) { bind<ProductService>() }
     single<MarketService> { MarketServiceImp() }
     single<CategoryService> { CategoryServiceImp() }
     single<MarketCategoriesService> { MarketCategoriesServiceImp() }
+
     single<ProductValidation> { ProductValidationImpl() }
     single<CategoryValidation> { CategoryValidationImpl() }
     single<DeleteAllTablesService> { DeleteAllTablesServiceServiceImp(get()) }
