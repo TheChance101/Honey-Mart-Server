f4433c0d9751d3400b0b53f8ee324cbbd9a30896
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-02 16:44:18 +0300

    create and implement validation logic for create market

--- app/core/src/main/java/com/thechance/core/data/validation/market/MarketValidation.kt
+++ app/core/src/main/java/com/thechance/core/data/validation/market/MarketValidation.kt
@@ -1,7 +1,8 @@
 package com.thechance.core.data.validation.market
 
-import com.thechance.api.model.Market
-
 interface MarketValidation {
-    fun createMarket(marketName:String):Market
+    fun isValidMarketName(name: String): Boolean
+    fun checkCreateValidation(marketName: String): Exception?
+    fun checkNameLength(marketName: String): Boolean
+
 }
