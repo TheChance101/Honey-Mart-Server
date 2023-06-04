ebe18771eaaeab8daa0020598ef18af5c94e925a
amnah.a <amnah44ali@gmail.com>
2023-06-04 18:04:12 +0300

    add wrap func to handle catch

--- app/ui/src/main/java/com/thechance/api/endpoints/Products.kt
+++ app/ui/src/main/java/com/thechance/api/endpoints/Products.kt
@@ -15,96 +15,86 @@ fun Route.productsRoutes(productService: ProductService) {
 
         get("/{productId}") {
             val productId = call.parameters["productId"]?.trim()?.toLongOrNull()
-            try {
+
+            onWrapper(call) {
                 val categories = productService.getAllCategoryForProduct(productId = productId)
                 call.respond(ServerResponse.success(categories))
-            } catch (e: InvalidInputException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            }catch (e:ItemNotAvailableException){
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
             }
         }
 
         post {
-            try {
-                val params = call.receiveParameters()
-                val productName = params["name"]?.trim().orEmpty()
-                val productPrice = params["price"]?.trim()?.toDoubleOrNull().orZero()
-                val productQuantity = params["quantity"]?.trim()
-                val categoriesId = params["categoriesId"]?.trim()?.toLongIds()
+            val params = call.receiveParameters()
+            val productName = params["name"]?.trim().orEmpty()
+            val productPrice = params["price"]?.trim()?.toDoubleOrNull().orZero()
+            val productQuantity = params["quantity"]?.trim()
+            val categoriesId = params["categoriesId"]?.trim()?.toLongIds()
 
+            onWrapper(call) {
                 val newAddedProduct = productService.create(productName, productPrice, productQuantity, categoriesId)
                 call.respond(HttpStatusCode.Created, ServerResponse.success(newAddedProduct))
-            } catch (e: InvalidInputException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, ServerResponse.error(e.message.toString()))
             }
         }
 
         put("{id}") {
-            try {
-                val productId = call.parameters["id"]?.trim()?.toLongOrNull()
-                val params = call.receiveParameters()
-                val productName = params["name"]?.trim()
-                val productPrice = params["price"]?.trim()?.toDoubleOrNull()
-                val productQuantity = params["quantity"]?.trim()
+            val productId = call.parameters["id"]?.trim()?.toLongOrNull()
+            val params = call.receiveParameters()
+            val productName = params["name"]?.trim()
+            val productPrice = params["price"]?.trim()?.toDoubleOrNull()
+            val productQuantity = params["quantity"]?.trim()
 
+            onWrapper(call) {
                 val updatedProduct = productService.updateProduct(
-                    productId = productId,
-                    productName = productName,
-                    productPrice = productPrice,
-                    productQuantity = productQuantity
+                        productId = productId,
+                        productName = productName,
+                        productPrice = productPrice,
+                        productQuantity = productQuantity
                 )
                 call.respond(HttpStatusCode.OK, ServerResponse.success(true, updatedProduct))
-            } catch (e: InvalidInputException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: ItemNotAvailableException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: IdNotFoundException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, ServerResponse.error(e.message.toString()))
             }
         }
 
         put("{id}/updateCategories") {
-            try {
-                val productId = call.parameters["id"]?.trim()?.toLongOrNull()
-                val params = call.receiveParameters()
-                val categoriesId = params["categoriesId"]?.trim().toLongIds()
+            val productId = call.parameters["id"]?.trim()?.toLongOrNull()
+            val params = call.receiveParameters()
+            val categoriesId = params["categoriesId"]?.trim().toLongIds()
 
+            onWrapper(call) {
                 val updatedProductCategory = productService.updateProductCategory(
-                    productId = productId, categoryIds = categoriesId
+                        productId = productId, categoryIds = categoriesId
                 )
                 call.respond(HttpStatusCode.OK, ServerResponse.success(updatedProductCategory))
-            } catch (e: InvalidInputException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: IdNotFoundException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, ServerResponse.error(e.message.toString()))
             }
         }
 
         delete("{id}") {
             val productId = call.parameters["id"]?.trim()?.toLongOrNull()
-            try {
+
+            onWrapper(call = call) {
                 val result = productService.deleteProduct(productId = productId)
                 call.respond(
-                    HttpStatusCode.OK,
-                    ServerResponse.success(result = result, successMessage = "Product Deleted successfully.")
+                        HttpStatusCode.OK,
+                        ServerResponse.success(result = result, successMessage = "Product Deleted successfully.")
                 )
-            } catch (e: InvalidInputException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: ItemNotAvailableException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: IdNotFoundException) {
-                call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, ServerResponse.error(e.message.toString()))
             }
         }
     }
 }
+
+suspend fun onWrapper(call: ApplicationCall, function: suspend () -> Unit) {
+    try {
+
+        function()
+
+    } catch (e: InvalidInputException) {
+        call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
+    } catch (e: ItemNotAvailableException) {
+        call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
+    } catch (e: IdNotFoundException) {
+        call.respond(e.statusCode, ServerResponse.error(e.message.toString()))
+    } catch (e: Exception) {
+        call.respond(HttpStatusCode.BadRequest, ServerResponse.error(e.message.toString()))
+    }
+}
+
+
 
