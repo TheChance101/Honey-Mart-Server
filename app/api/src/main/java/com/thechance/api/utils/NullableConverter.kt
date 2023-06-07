10046047c703d04537c93fa5ed83bcb224ab8dc5
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-07 18:30:56 +0300

    add endpoint and related helper functions & models

--- app/api/src/main/java/com/thechance/api/utils/NullableConverter.kt
+++ app/api/src/main/java/com/thechance/api/utils/NullableConverter.kt
@@ -1,6 +1,18 @@
 package com.thechance.api.utils
 
+import com.thechance.api.model.CreateOrderRequest
+import com.thechance.core.data.model.OrderItem
+
 fun Double?.orZero() = this ?: 0.0
 
 
 fun String?.toLongIds() = this?.split(",")?.map { it.toLongOrNull() ?: 0L } ?: emptyList()
+
+internal fun List<CreateOrderRequest.OrderItemRequest>?.toOrderItems(): List<OrderItem> {
+    return this?.map {
+        OrderItem(
+            it.productId.toLong(),
+            it.quantity.toInt()
+        )
+    } ?: emptyList()
+}
