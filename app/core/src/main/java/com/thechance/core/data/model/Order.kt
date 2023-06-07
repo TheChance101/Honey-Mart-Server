30e2fdcb15e91db4f9d2a368056df5f92c94ea2c
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-07 18:26:41 +0300

    create models for order

---
+++ app/core/src/main/java/com/thechance/core/data/model/Order.kt
@@ -0,0 +1,12 @@
+package com.thechance.core.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(
+    val id: Long,
+    val marketId:Long,
+    val orderDate:String,
+    val totalPrice: Double,
+    val isPaid: Boolean
+)
