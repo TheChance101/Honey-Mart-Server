7e3027b87d28c6e36bb75aacaba421769bac15dd
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-07 18:30:22 +0300

    add order use cases container

---
+++ app/core/src/main/java/com/thechance/core/data/usecase/order/OrderUseCasesContainer.kt
@@ -0,0 +1,8 @@
+package com.thechance.core.data.usecase.order
+
+import org.koin.core.component.KoinComponent
+
+data class OrderUseCasesContainer(
+    val createOrderUseCase: CreateOrderUseCase,
+    val getOrdersForMarketUseCase: GetOrdersForMarketUseCase
+):KoinComponent
