6aa501f7d141a20d2adfb6a70714dc58bb6ec7d3
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-08 09:14:56 +0300

    implement TokenConfig

--- app/core/src/main/java/com/thechance/core/data/security/token/TokenConfig.kt
+++ app/core/src/main/java/com/thechance/core/data/security/token/TokenConfig.kt
@@ -1,3 +1,8 @@
 package com.thechance.core.data.security.token
 
-data class TokenConfig()
+data class TokenConfig(
+    val issuer: String,
+    val audience: String,
+    val expiresIn: Long,
+    val secret: String
+)
