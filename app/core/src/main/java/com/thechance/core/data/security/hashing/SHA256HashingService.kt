92e94055405e40c05fb6f3262240a56c0790e9d8
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-10 11:54:12 -0700

    refactor token

--- app/core/src/main/java/com/thechance/core/data/security/hashing/SHA256HashingService.kt
+++ app/core/src/main/java/com/thechance/core/data/security/hashing/SHA256HashingService.kt
@@ -1,11 +1,12 @@
 package com.thechance.core.data.security.hashing
 
+import com.thechance.core.data.repository.security.HashingService
 import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils
 import org.koin.core.component.KoinComponent
 import java.security.SecureRandom
 
-class SHA256HashingService: HashingService , KoinComponent {
+class SHA256HashingService: HashingService, KoinComponent {
     override fun generateSaltedHash(value: String, saltLength: Int): SaltedHash {
         val salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLength)
         val saltAsHex = Hex.encodeHexString(salt)
