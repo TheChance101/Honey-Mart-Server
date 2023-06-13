2797d4af5e29bd7aa3796826469119f628e6dd5f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-13 15:57:42 +0300

    implement GetOrdersForUserUseCase

--- app/core/src/main/java/com/thechance/core/domain/usecase/order/GetOrdersForUserUseCase.kt
+++ app/core/src/main/java/com/thechance/core/domain/usecase/order/GetOrdersForUserUseCase.kt
@@ -1,4 +1,17 @@
 package com.thechance.core.domain.usecase.order
 
-class GetOrdersForUserUseCase {
+import com.thechance.core.domain.repository.HoneyMartRepository
+import com.thechance.core.entity.Order
+import com.thechance.core.utils.InvalidUserIdException
+import com.thechance.core.utils.isInvalidId
+import org.koin.core.component.KoinComponent
+
+class GetOrdersForUserUseCase(private val repository: HoneyMartRepository) : KoinComponent {
+    suspend operator fun invoke(userId: Long?): List<Order> {
+        return if (isInvalidId(userId)) {
+            throw InvalidUserIdException()
+        } else {
+            repository.getAllOrdersForUser(userId!!)
+        }
+    }
 }
