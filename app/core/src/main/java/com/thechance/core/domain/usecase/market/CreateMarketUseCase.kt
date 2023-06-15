e42e7a754356863f698affd9bf390dea623a5968
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-14 20:21:18 -0700

    fix market,category name.

--- app/core/src/main/java/com/thechance/core/domain/usecase/market/CreateMarketUseCase.kt
+++ app/core/src/main/java/com/thechance/core/domain/usecase/market/CreateMarketUseCase.kt
@@ -3,16 +3,17 @@ package com.thechance.core.domain.usecase.market
 import com.thechance.core.domain.repository.HoneyMartRepository
 import com.thechance.core.entity.Market
 import com.thechance.core.utils.InvalidMarketNameException
-import com.thechance.core.utils.isValidUsername
+import com.thechance.core.utils.isValidateMarketName
 import org.koin.core.component.KoinComponent
 
 class CreateMarketUseCase(private val repository: HoneyMartRepository) : KoinComponent {
 
     suspend operator fun invoke(marketName: String?): Market {
-        return if (isValidUsername(marketName)) {
+        return if (!isValidateMarketName(marketName)) {
             throw InvalidMarketNameException()
         } else {
             repository.createMarket(marketName!!)
         }
     }
+
 }
