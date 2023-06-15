e7a72a9c04b15cbfb639a0bf6fe25fa8a65e646d
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-15 14:27:50 -0700

    change type date

--- app/core/src/main/java/com/thechance/core/data/datasource/database/tables/order/OrderTable.kt
+++ app/core/src/main/java/com/thechance/core/data/datasource/database/tables/order/OrderTable.kt
@@ -4,13 +4,13 @@ import com.thechance.core.data.datasource.database.tables.MarketTable
 import com.thechance.core.data.datasource.database.tables.NormalUserTable
 import com.thechance.core.utils.ORDER_STATE_IN_PROGRESS
 import org.jetbrains.exposed.dao.id.LongIdTable
-import org.jetbrains.exposed.sql.javatime.date
-import java.time.LocalDate
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
 
 object OrderTable : LongIdTable() {
     val userId = reference("userId", NormalUserTable)
     val marketId = reference("marketId", MarketTable)
-    val orderDate = date("orderDate").default(LocalDate.now())
+    val orderDate = datetime("orderDate").clientDefault { LocalDateTime.now() }
     val totalPrice = double("totalPrice")
     val state = integer("state").default(ORDER_STATE_IN_PROGRESS)
 }
