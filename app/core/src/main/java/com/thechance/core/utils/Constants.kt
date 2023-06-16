4d8058c1372fa7032769adb088daa01f652e96f4
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-16 19:43:16 +0300

    add related constants

--- app/core/src/main/java/com/thechance/core/utils/Constants.kt
+++ app/core/src/main/java/com/thechance/core/utils/Constants.kt
@@ -9,3 +9,6 @@ internal const val ORDER_STATE_IN_PROGRESS = 0
 internal const val ORDER_STATE_IN_DONE = 1
 
 const val ROLE_TYPE = "ROLE_TYPE"
+
+const val BASE_URL = "http://127.0.0.1:8080"
+const val USER_IMAGES_PATH = "files/image_uploads"
