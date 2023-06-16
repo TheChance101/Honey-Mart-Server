a0a510139abd2a8ea5890d0c3468dae254895d12
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-15 17:37:54 -0700

    convertDateToMillis

--- app/api/src/main/java/com/thechance/api/utils/ConvertDate.kt
+++
@@ -1,7 +0,0 @@
-package com.thechance.api.utils
-
-import java.time.LocalDateTime
-
-fun LocalDateTime.convertDateToMillis(): Long {
-    return this.atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli()
-}
