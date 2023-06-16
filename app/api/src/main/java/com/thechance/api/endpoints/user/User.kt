f06331d8908f03209c6578db96e9ad21c06f79fb
Nour Elden Alaa <nourelden515@gmail.com>
2023-06-16 19:48:36 +0300

    add image endpoints to user routing

--- app/api/src/main/java/com/thechance/api/endpoints/user/User.kt
+++ app/api/src/main/java/com/thechance/api/endpoints/user/User.kt
@@ -2,8 +2,12 @@ package com.thechance.api.endpoints.user
 
 import com.thechance.api.ServerResponse
 import com.thechance.core.domain.usecase.user.UserUseCaseContainer
+import com.thechance.core.utils.ROLE_TYPE
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -32,6 +36,23 @@ fun Route.userRoutes() {
 
             val token = userUseCasesContainer.verifyUserUseCase(email, password)
             call.respond(HttpStatusCode.Created, ServerResponse.success(token, "Logged in Successfully"))
+        }
+        authenticate {
+            post("/profileImage") {
+                val principal = call.principal<JWTPrincipal>()
+                val userId = principal?.payload?.subject?.toLongOrNull()
+                val role = principal?.getClaim(ROLE_TYPE, String::class)
+                val imageParts = call.receiveMultipart().readAllParts()
+                val imageUrl = userUseCasesContainer.saveUserProfileImageUseCase(imageParts, userId, role)
+                call.respond(HttpStatusCode.Created, ServerResponse.success("Added successfully", imageUrl))
+            }
+            get("/profileImage") {
+                val principal = call.principal<JWTPrincipal>()
+                val userId = principal?.payload?.subject?.toLongOrNull()
+                val role = principal?.getClaim(ROLE_TYPE, String::class)
+                val image = userUseCasesContainer.getUserProfileImageUseCase(userId, role)
+                call.respond(HttpStatusCode.Created, ServerResponse.success("Get Image successfully", image))
+            }
         }
     }
 }
