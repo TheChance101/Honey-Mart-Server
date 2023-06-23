446873db20db0ca3341b1eee8b1755f9057c2b02
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-06-23 03:36:16 +0300

    add images to data models mappers

--- app/api/src/main/java/com/thechance/api/model/ProductWithCount.kt
+++ app/api/src/main/java/com/thechance/api/model/ProductWithCount.kt
@@ -1,8 +1,15 @@
 package com.thechance.api.model
 
 import kotlinx.serialization.Serializable
+import java.awt.Image
 
 
 @Serializable
-data class ProductWithCount(val id: Long, val name: String, val count: Int, val price: Double)
+data class ProductWithCount(
+    val id: Long,
+    val name: String,
+    val count: Int,
+    val price: Double,
+    val images:List<String>
+    )
 
--- app/core/src/main/java/com/thechance/core/entity/ProductInCart.kt
+++ app/core/src/main/java/com/thechance/core/entity/ProductInCart.kt
@@ -1,4 +1,10 @@
 package com.thechance.core.entity
 
-data class ProductInCart(val id: Long, val name: String, val count: Int, val price: Double)
+data class ProductInCart(
+    val id: Long,
+    val name: String,
+    val count: Int,
+    val price: Double,
+    val images:List<Image>
+    )
 
