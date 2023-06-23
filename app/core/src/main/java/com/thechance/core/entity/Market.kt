def65d84f518c9b047753b01a7be395f7324da40
Nada Feteiha <nada.feteiha@gmail.com>
2023-06-23 14:04:21 -0700

    add market details model

--- app/core/src/main/java/com/thechance/core/entity/Market.kt
+++
@@ -1,4 +0,0 @@
-package com.thechance.core.entity
-
-
-data class Market(val marketId: Long, val marketName: String, val imageUrl: String)
---
+++ app/core/src/main/java/com/thechance/core/entity/market/Market.kt
@@ -0,0 +1,4 @@
+package com.thechance.core.entity.market
+
+
+data class Market(val marketId: Long, val marketName: String, val imageUrl: String, val isDeleted: Boolean)
