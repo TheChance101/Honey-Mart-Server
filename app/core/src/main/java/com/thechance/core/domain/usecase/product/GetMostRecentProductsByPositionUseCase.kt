7e50e4f11000c3333d9444c3de1cefc58ca97bb7
Elboray <elboray456@gmail.com>
2023-08-11 15:29:11 +0300

    refactor use case

--- app/core/src/main/java/com/thechance/core/domain/usecase/product/GetMostRecentProductsByPositionUseCase.kt
+++ app/core/src/main/java/com/thechance/core/domain/usecase/product/GetMostRecentProductsByPositionUseCase.kt
@@ -4,8 +4,8 @@ import com.thechance.core.domain.repository.HoneyMartRepository
 import com.thechance.core.entity.Product
 import org.koin.core.component.KoinComponent
 
-class GetMostRecentProductsByPositionUseCase(private val repository: HoneyMartRepository) : KoinComponent {
-
+class GetMostRecentProductsByPositionUseCase(private val repository: HoneyMartRepository) :
+    KoinComponent {
     suspend operator fun invoke(): List<Product> {
         return repository.getMostRecentProductsByPosition()
     }
