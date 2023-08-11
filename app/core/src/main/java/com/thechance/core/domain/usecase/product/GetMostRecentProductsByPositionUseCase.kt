67f350e0c68be0463266750cd6fe8fd25aab2358
Elboray <elboray456@gmail.com>
2023-08-11 15:17:58 +0300

    add product use case

---
+++ app/core/src/main/java/com/thechance/core/domain/usecase/product/GetMostRecentProductsByPositionUseCase.kt
@@ -0,0 +1,14 @@
+package com.thechance.core.domain.usecase.product
+
+import com.thechance.core.domain.repository.HoneyMartRepository
+import com.thechance.core.entity.Product
+import org.koin.core.component.KoinComponent
+
+class GetMostRecentProductsByPositionUseCase(private val repository: HoneyMartRepository) : KoinComponent {
+
+    suspend operator fun invoke(): List<Product> {
+        return repository.getMostRecentProductsByPosition()
+    }
+
+
+}
