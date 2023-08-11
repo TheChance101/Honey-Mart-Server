ce003b882e1a04007315c1cdd2b6af13fea10bd7
Elboray <elboray456@gmail.com>
2023-08-11 23:43:29 +0300

    add page for GetMostRecentProductsUseCase

--- app/core/src/main/java/com/thechance/core/domain/usecase/product/GetMostRecentProductsByPositionUseCase.kt
+++ app/core/src/main/java/com/thechance/core/domain/usecase/product/GetMostRecentProductsByPositionUseCase.kt
@@ -4,10 +4,10 @@ import com.thechance.core.domain.repository.HoneyMartRepository
 import com.thechance.core.entity.Product
 import org.koin.core.component.KoinComponent
 
-class GetMostRecentProductsByPositionUseCase(private val repository: HoneyMartRepository) :
+class GetMostRecentProductsUseCase(private val repository: HoneyMartRepository) :
     KoinComponent {
-    suspend operator fun invoke(): List<Product> {
-        return repository.getMostRecentProductsByPosition()
+    suspend operator fun invoke(page:Int): List<Product> {
+        return repository.getMostRecentProducts(page)
     }
 
 
