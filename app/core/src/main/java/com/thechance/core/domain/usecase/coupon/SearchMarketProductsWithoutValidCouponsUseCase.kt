63ad367501dc3d74eb080f05b5bde02ec676be0f
Nour Elden Alaa <nourelden515@gmail.com>
2023-08-28 01:49:57 +0300

    search marketProducts without valid coupons

---
+++ app/core/src/main/java/com/thechance/core/domain/usecase/coupon/SearchMarketProductsWithoutValidCouponsUseCase.kt
@@ -0,0 +1,35 @@
+package com.thechance.core.domain.usecase.coupon
+
+import com.thechance.core.domain.repository.HoneyMartRepository
+import com.thechance.core.entity.Product
+import com.thechance.core.utils.*
+import org.koin.core.component.KoinComponent
+
+class SearchMarketProductsWithoutValidCouponsUseCase(private val repository: HoneyMartRepository) : KoinComponent {
+    suspend operator fun invoke(ownerId: Long?, role: String?, productName: String?): List<Product> {
+
+        val marketId = repository.getMarketIdByOwnerId(ownerId = ownerId!!)
+
+        return when {
+            isInvalidId(ownerId) || !isValidRole(MARKET_OWNER_ROLE, role) -> {
+                throw InvalidOwnerIdException()
+            }
+
+            productName.isNullOrEmpty() -> {
+                throw MissingQueryParameterException()
+            }
+
+            !isValidQuery(productName) -> {
+                throw InvalidProductNameException()
+            }
+
+            marketId == null -> {
+                throw InvalidMarketIdException()
+            }
+
+            else -> {
+                repository.searchMarketProductsWithoutValidCoupons(marketId, productName)
+            }
+        }
+    }
+}
